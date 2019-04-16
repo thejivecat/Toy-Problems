f73de5d8fe1497620e682c20c55449612d759573
Srujan Bethi <ssbethi@gmail.com>
2019-04-16 09:49:31 -0600

    Finished rockpaperscissors with proper combinations

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,7 +18,17 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function () {
   // TODO: your solution here
+  var moves = ["R", "P", "S"]
+  var results = [];
+  for (let i = 0; i < moves.length; i++) {
+    for (let j = 0; j < moves.length; j++) {
+      for (let k = 0; k < moves.length; k++) {
+        var temp = moves[i].concat(moves[j], moves[k]);
+        results.push(temp);
+      }
+    }
+  }
+  return results;
 };
