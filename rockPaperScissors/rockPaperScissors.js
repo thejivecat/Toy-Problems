9cfd20f4a774cf09eac44f69a96f9846a269b39e
Fred Zirdung <fred.zirdung@gmail.com>
2019-04-15 22:39:24 -0700

    Correction to instructions

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -3,17 +3,18 @@
 * player could throw over a three-round game of rock-paper-scissors.
 *
 * Your output should look something like:
-*   [["rock", "rock", "rock"],
-*    ["rock", "rock", "paper"],
-*    ["rock", "rock", "scissors"],
-*    ["rock", "paper", "rock"],
-             ...etc...
-     ]
+*   ["RRR",
+*    "RRP",
+*    "RRS",
+*    "RPR",
+*    ...etc...
+*   ]
 *
 * Extra credit:
 *   - Make your function return answers for any number of rounds.
+*
 * Example:
-* rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
+* rockPaperScissors(5); // => ['RRRRR', 'RRRRP', 'RRRRS', etc...]
 *
 */
 
