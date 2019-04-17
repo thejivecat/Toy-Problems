77be76f76d28b3e6318f8ce1715990b3fc9ea5e0
Srujan Bethi <ssbethi@gmail.com>
2019-04-17 10:43:47 -0600

    Passed all tests for evenOccurence

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -1,6 +1,6 @@
 /*
  * Find the first item that occurs an even number of times in an array.
- * Remember to handle multiple even-occurrence items and return the first one. 
+ * Remember to handle multiple even-occurrence items and return the first one.
  * Return null if there are no even-occurrence items.
 */
 
@@ -9,7 +9,17 @@
  * var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
  * console.log(onlyEven); //  4
 */
-
 var evenOccurrence = function(arr) {
-  // Your code here.
+  var evens = {};
+  for (let i = 0; i < arr.length; i++) {
+    evens[arr[i]] = 0;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    evens[arr[i]] ++;
+  }
+  for (var key in evens) {
+    if (evens[key]%2===0) {
+      return key;
+    }
+  }
 };
